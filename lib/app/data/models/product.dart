0822fc45c3fdd28b5c913f2a478b4e12a300d88e
Apit <apit@thezatapp.com>
2022-12-01 20:16:10 +0700

    add product model

---
+++ lib/app/data/models/product.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+
+List<Product> productFromJson(String str) => List<Product>.from(json.decode(str).map((x) => Product.fromJson(x)));
+
+String productToJson(List<Product> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Product {
+    Product({
+        this.id,
+        this.title,
+        this.price,
+        this.description,
+        this.category,
+        this.image,
+        this.rating,
+    });
+
+    final int? id;
+    final String? title;
+    final double? price;
+    final String? description;
+    final String? category;
+    final String? image;
+    final Rating? rating;
+
+    factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json["id"],
+        title: json["title"],
+        price: json["price"].toDouble(),
+        description: json["description"],
+        category: json["category"],
+        image: json["image"],
+        rating: Rating.fromJson(json["rating"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "price": price,
+        "description": description,
+        "category": category,
+        "image": image,
+        "rating": rating!.toJson(),
+    };
+}
+
+class Rating {
+    Rating({
+        this.rate,
+        this.count,
+    });
+
+    final double? rate;
+    final int? count;
+
+    factory Rating.fromJson(Map<String, dynamic> json) => Rating(
+        rate: json["rate"].toDouble(),
+        count: json["count"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "rate": rate,
+        "count": count,
+    };
+}
